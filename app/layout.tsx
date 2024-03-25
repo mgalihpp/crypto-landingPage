bae1433daf85e8ea8ef317246ddcc0bed2937461
mgalihpp <muhammadgalih451@gmail.com>
2024-03-26 05:46:18 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Banner/Footer";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "CrCrypto",
